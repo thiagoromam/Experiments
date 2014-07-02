8b73bf55fd8926cd296d876679303c6bb02f6aa0
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2014-07-01 22:44:15 -0300

    initial test of TypeScript + AngularJs

---
+++ TypeScriptPlusAngularJs/library/greeter.ts
@@ -0,0 +1,24 @@
+﻿/// <reference path="jquery.d.ts"/>
+
+interface Person {
+    firstname: string;
+    lastname: string;
+}
+
+class Student {
+    fullname: string;
+
+    constructor(public firstname, public middleinitial, public lastname) {
+        this.fullname = firstname + " " + middleinitial + " " + lastname;
+    }
+}
+
+function greeter(person: Person) {
+    return "Hello, " + person.firstname + " " + person.lastname;
+}
+
+var user = new Student("Jane", "M.", "User");
+
+$(function () {
+    $("#userPanel").html(greeter(user));
+});
