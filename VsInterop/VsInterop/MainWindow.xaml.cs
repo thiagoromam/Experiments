3c7866069cc252c71c6bc47d0ca0e89f3ad226cf
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2014-10-07 01:20:19 -0300

    create solution programmatically

---
+++ VsInterop/VsInterop/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using EnvDTE;
+using EnvDTE100;
+
+// http://www.viva64.com/en/b/0169/
+// http://msdn.microsoft.com/en-us/library/y849h0w1.aspx
+// http://msdn.microsoft.com/en-us/library/1xt0ezx9.aspx
+
+namespace VsInterop
+{
+    public partial class MainWindow
+    {
+        private readonly string _solutionFolder;
+        private const string SolutionName = "GeneratedSolution";
+        private const string ProjectName = "SharedContent";
+        private DTE _ide;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeVisualStudio();
+            _solutionFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), SolutionName + @"\");
+
+            Closed += (s, e) => CloseVisualStudio();
+        }
+
+        void OnCreateSolutionClick(object sender, RoutedEventArgs e)
+        {
+            using (new MessageFilter())
+            {
+                var sln = (Solution4)_ide.Solution;
+
+                sln.Create(_solutionFolder, SolutionName);
+
+                var projectTemplate = sln.GetProjectTemplate(@"Store Apps\Universal Apps\1033\SharedEmptyMaster\SharedEmptyMaster.vstemplate", "CSharp");
+                var projectPath = Path.Combine(_solutionFolder, ProjectName + @"\");
+                sln.AddFromTemplate(projectTemplate, projectPath, ProjectName, false);
+
+                _ide.ExecuteCommand("File.SaveAll");
+
+                MessageBox.Show("Solution Created");
+            }
+        }
+        void OnShowSolutionInfoClick(object sender, RoutedEventArgs e)
+        {
+            using (new MessageFilter())
+            {
+                var sln = GetSolution();
+
+                var message = string.Format("Projects Count: {0}", sln.Projects.Count);
+
+                foreach (Project project in sln.Projects)
+                {
+                    message += "\n  Project: " + project.Name;
+                    message = project.ProjectItems.Cast<ProjectItem>().Aggregate(message, (m, i) => m + ("\n      " + i.Name));
+                }
+
+                MessageBox.Show(message);
+            }
+        }
+        void OnAddClassClick(object sender, RoutedEventArgs e)
+        {
+            if (!Regex.IsMatch(ClassName.Text, "^[A-Z][A-z0-9]+$"))
+            {
+                MessageBox.Show("Inform a valid class name");
+                return;
+            }
+
+            using (new MessageFilter())
+            {
+                var sln = GetSolution();
+
+                var className = ClassName.Text + ".cs";
+                var classTemplate = sln.GetProjectItemTemplate("Class.zip", "CSharp");
+                var sharedProject = sln.Projects.Cast<Project>().Single(f => f.Name == ProjectName);
+                sharedProject.ProjectItems.AddFromTemplate(classTemplate, className);
+
+                _ide.ExecuteCommand("File.SaveAll");
+
+                MessageBox.Show(className + " added!");
+            }
+        }
+
+        Solution4 GetSolution()
+        {
+            if (!_ide.Solution.IsOpen)
+                _ide.Solution.Open(Path.Combine(_solutionFolder, SolutionName + ".sln"));
+
+            return (Solution4)_ide.Solution;
+        }
+        void InitializeVisualStudio()
+        {
+            var type = Type.GetTypeFromProgID("VisualStudio.DTE.12.0", true);
+            _ide = (DTE)Activator.CreateInstance(type, true);
+        }
+        void CloseVisualStudio()
+        {
+            _ide.Quit();
+        }
+    }
+}
---
+++ VsInterop/VsInterop/MessageFilter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace VsInterop
+{
+    [ComImport()]
+    [Guid("00000016-0000-0000-C000-000000000046")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IMessageFilter
+    {
+        [PreserveSig]
+        int HandleInComingCall(
+          int dwCallType,
+          IntPtr hTaskCaller,
+          int dwTickCount,
+          IntPtr lpInterfaceInfo);
+
+        [PreserveSig]
+        int RetryRejectedCall(
+          IntPtr hTaskCallee,
+          int dwTickCount,
+          int dwRejectType);
+
+        [PreserveSig]
+        int MessagePending(
+          IntPtr hTaskCallee,
+          int dwTickCount,
+          int dwPendingType);
+    }
+
+    class MessageFilter : MarshalByRefObject, IDisposable, IMessageFilter
+    {
+
+        [DllImport("ole32.dll")]
+        [PreserveSig]
+        private static extern int CoRegisterMessageFilter(
+          IMessageFilter lpMessageFilter,
+          out IMessageFilter lplpMessageFilter);
+
+        private IMessageFilter oldFilter;
+        private const int SERVERCALL_ISHANDLED = 0;
+        private const int PENDINGMSG_WAITNOPROCESS = 2;
+        private const int SERVERCALL_RETRYLATER = 2;
+
+        public MessageFilter()
+        {
+            //Starting IMessageFilter for COM objects
+            int hr =
+              MessageFilter.CoRegisterMessageFilter(
+                (IMessageFilter)this,
+                 out this.oldFilter);
+            System.Diagnostics.Debug.Assert(hr >= 0,
+              "Registering COM IMessageFilter failed!");
+        }
+
+        public void Dispose()
+        {
+            //disabling IMessageFilter
+            IMessageFilter dummy;
+            int hr = MessageFilter.CoRegisterMessageFilter(this.oldFilter,
+                                                           out dummy);
+            System.Diagnostics.Debug.Assert(hr >= 0, "De-Registering COM IMessageFilter failed!");
+            System.GC.SuppressFinalize(this);
+        }
+
+        int IMessageFilter.HandleInComingCall(int dwCallType,
+          IntPtr threadIdCaller, int dwTickCount, IntPtr lpInterfaceInfo)
+        {
+            // Return the ole default (don't let the call through).
+            return MessageFilter.SERVERCALL_ISHANDLED;
+        }
+
+        int IMessageFilter.RetryRejectedCall(IntPtr threadIDCallee,
+          int dwTickCount, int dwRejectType)
+        {
+            if (dwRejectType == MessageFilter.SERVERCALL_RETRYLATER)
+            {
+                // Retry the thread call immediately if return >=0 & 
+                // <100.
+                return 150; //waiting 150 mseconds until retry
+            }
+            // Too busy; cancel call. SERVERCALL_REJECTED
+            return -1;
+            //Call was rejected by callee. 
+            //(Exception from HRESULT: 0x80010001 (RPC_E_CALL_REJECTED))
+        }
+
+        int IMessageFilter.MessagePending(
+            IntPtr threadIDCallee, int dwTickCount, int dwPendingType)
+        {
+            // Perform default processing.
+            return MessageFilter.PENDINGMSG_WAITNOPROCESS;
+        }
+    }
+}
