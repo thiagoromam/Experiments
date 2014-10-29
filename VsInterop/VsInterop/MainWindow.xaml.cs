90ed5679d27248b36f06273cf884f5c15ec74f64
Thiago Luis Romam Matheus <thiago.romam@outlook.com>
2014-10-29 16:06:30 -0200

    class generation via code model

--- VsInterop/VsInterop/MainWindow.xaml.cs
+++ VsInterop/VsInterop/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text.RegularExpressions;
 using System.Windows;
 using EnvDTE;
 using EnvDTE100;
+using EnvDTE80;
 
 namespace VsInterop
 {
@@ -25,78 +26,137 @@ namespace VsInterop
 
             InitializeVisualStudio();
             _solutionFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), SolutionName + @"\");
+            OnShowVisualStudioClick(null, null);
 
             Closed += (s, e) => CloseVisualStudio();
         }
 
+        void OnOpenSolutionClick(object sender, RoutedEventArgs e)
+        {
+            TryExecute(() =>
+            {
+                using (new MessageFilter())
+                    GetSolution();
+            });
+        }
         void OnCreateSolutionClick(object sender, RoutedEventArgs e)
         {
-            using (new MessageFilter())
+            TryExecute(() =>
             {
-                var sln = (Solution4)_ide.Solution;
+                using (new MessageFilter())
+                {
+                    var sln = (Solution4)_ide.Solution;
 
-                sln.Create(_solutionFolder, SolutionName);
+                    sln.Create(_solutionFolder, SolutionName);
 
-                var sharedProjectTemplate = sln.GetProjectTemplate(@"Store Apps\Universal Apps\1033\SharedEmptyMaster\SharedEmptyMaster.vstemplate", "CSharp");
-                var sharedProjectPath = Path.Combine(_solutionFolder, SharedProjectName + @"\");
-                sln.AddFromTemplate(sharedProjectTemplate, sharedProjectPath, SharedProjectName, false);
+                    // Shared Project
+                    var sharedProjectTemplate = sln.GetProjectTemplate(@"Store Apps\Universal Apps\1033\SharedEmptyMaster\SharedEmptyMaster.vstemplate", "CSharp");
+                    var sharedProjectPath = Path.Combine(_solutionFolder, SharedProjectName + @"\");
+                    sln.AddFromTemplate(sharedProjectTemplate, sharedProjectPath, SharedProjectName, false);
 
-                var sharedProject = sln.Projects.Item(1);
-                var sharedProjectNamespace = sharedProject.Properties.Cast<Property>().Single(p => p.Name == "RootNamespace");
-                sharedProjectNamespace.Value = SharedProjectName;
+                    var sharedProject = sln.Projects.Item(1);
+                    var sharedProjectNamespace = sharedProject.Properties.Cast<Property>().Single(p => p.Name == "RootNamespace");
+                    sharedProjectNamespace.Value = SharedProjectName;
 
-                var monogameProjectTemplate = sln.GetProjectTemplate(@"MonoGame\Windows.zip", "CSharp");
-                const string monogameProjectName = "MonogameProject";
-                var monogameProjectPath = Path.Combine(_solutionFolder, monogameProjectName + @"\");
-                sln.AddFromTemplate(monogameProjectTemplate, monogameProjectPath, monogameProjectName, false);
+                    // Monogame Project
+                    var monogameProjectTemplate = sln.GetProjectTemplate(@"MonoGame\Windows.zip", "CSharp");
+                    const string monogameProjectName = "MonogameProject";
+                    var monogameProjectPath = Path.Combine(_solutionFolder, monogameProjectName + @"\");
+                    sln.AddFromTemplate(monogameProjectTemplate, monogameProjectPath, monogameProjectName, false);
 
-                _ide.ExecuteCommand("File.SaveAll");
+                    // Save
+                    _ide.ExecuteCommand("File.SaveAll");
 
-                MessageBox.Show("Solution Created");
-            }
+                    MessageBox.Show("Solution Created");
+                }
+            });
         }
         void OnShowSolutionInfoClick(object sender, RoutedEventArgs e)
         {
-            using (new MessageFilter())
+            TryExecute(() =>
             {
-                var sln = GetSolution();
-
-                var message = string.Format("Projects Count: {0}", sln.Projects.Count);
-
-                foreach (Project project in sln.Projects)
+                using (new MessageFilter())
                 {
-                    message += "\n  Project: " + project.Name;
-                    message = project.ProjectItems.Cast<ProjectItem>().Aggregate(message, (m, i) => m + ("\n      " + i.Name));
-                }
+                    var sln = GetSolution();
 
-                MessageBox.Show(message);
-            }
+                    var message = string.Format("Projects Count: {0}", sln.Projects.Count);
+
+                    foreach (Project project in sln.Projects)
+                    {
+                        message += "\n  Project: " + project.Name;
+                        message = project.ProjectItems.Cast<ProjectItem>().Aggregate(message, (m, i) => m + ("\n      " + i.Name));
+                    }
+
+                    MessageBox.Show(message);
+                }
+            });
         }
         void OnAddClassClick(object sender, RoutedEventArgs e)
         {
-            if (!Regex.IsMatch(ClassName.Text, "^[A-Z][A-z0-9]+$"))
+            TryExecute(() =>
             {
-                MessageBox.Show("Inform a valid class name");
-                return;
-            }
+                if (!Regex.IsMatch(ClassName.Text, "^[A-Z][A-z0-9]+$"))
+                {
+                    MessageBox.Show("Inform a valid class name");
+                    return;
+                }
 
-            using (new MessageFilter())
-            {
-                var sln = GetSolution();
+                using (new MessageFilter())
+                {
+                    var sln = GetSolution();
 
-                var className = ClassName.Text + ".cs";
-                var classTemplate = sln.GetProjectItemTemplate("Class.zip", "CSharp");
-                var sharedProject = sln.Projects.Cast<Project>().Single(f => f.Name == SharedProjectName);
-                sharedProject.ProjectItems.AddFromTemplate(classTemplate, className);
+                    var className = ClassName.Text + ".cs";
+                    var classTemplate = sln.GetProjectItemTemplate("Class.zip", "CSharp");
+                    var sharedProject = sln.Projects.Cast<Project>().Single(f => f.Name == SharedProjectName);
+                    sharedProject.ProjectItems.AddFromTemplate(classTemplate, className);
 
-                _ide.ExecuteCommand("File.SaveAll");
+                    _ide.ExecuteCommand("File.SaveAll");
 
-                MessageBox.Show(className + " added!");
-            }
+                    MessageBox.Show(className + " added!");
+                }
+            });
         }
         void OnShowVisualStudioClick(object sender, RoutedEventArgs e)
         {
-            _ide.UserControl = true;
+            TryExecute(() =>
+            {
+                using (new MessageFilter())
+                    _ide.UserControl = true;
+            });
+        }
+        void OnAddSampleClassClick(object sender, RoutedEventArgs e)
+        {
+            TryExecute(() =>
+            {
+                using (new MessageFilter())
+                {
+                    var sln = GetSolution();
+
+                    var sharedProject = sln.Projects.Item(1);
+                    
+                    // Add Class via CodeModel
+                    var csItemTemplatePath = sln.GetProjectItemTemplate("CodeFile", "CSharp");
+                    sharedProject.ProjectItems.AddFromTemplate(csItemTemplatePath, "GameObject.cs");
+
+                    // Generate Class content
+                    var testClass = sharedProject.ProjectItems.Item("GameObject.cs");
+                    var similarProjectItems = SolutionHelper.GetSimilarProjectItems((Solution)sln, testClass);
+                    var fileCodeModel = (FileCodeModel2)similarProjectItems.Select(x => x.FileCodeModel).First(y => y != null);
+
+                    fileCodeModel.AddImport("Microsoft.Xna.Framework");
+                    
+                    var @namespace = fileCodeModel.AddNamespace(SharedProjectName);
+                    var @class = (CodeClass2)@namespace.AddClass("GameObject");
+
+                    var function = (CodeFunction2)@class.AddFunction("Update", vsCMFunction.vsCMFunctionFunction, "void", Access: vsCMAccess.vsCMAccessPublic);
+                    function.AddParameter("gameTime", "GameTime");
+
+                    // https://social.msdn.microsoft.com/Forums/vstudio/pt-BR/faad34d5-e046-494c-81c3-7b34483c3a7f/adding-autoimplemented-property-using-codeclass2?forum=vsx
+                    //var property = (CodeProperty2)@class.AddProperty("Position", "Position", "Vector2", -1, vsCMAccess.vsCMAccessPublic, null);
+
+                    testClass.Save();
+                }
+            });
         }
 
         Solution4 GetSolution()
@@ -108,15 +168,30 @@ namespace VsInterop
         }
         void InitializeVisualStudio()
         {
-            var type = Type.GetTypeFromProgID("VisualStudio.DTE.12.0", true);
-            _ide = (DTE)Activator.CreateInstance(type, true);
+            TryExecute(() =>
+            {
+                var type = Type.GetTypeFromProgID("VisualStudio.DTE.12.0", true);
+                _ide = (DTE)Activator.CreateInstance(type, true);
 
-            _ideEvents = _ide.Events.DTEEvents;
-            _ideEvents.OnBeginShutdown += InitializeVisualStudio;
+                _ideEvents = _ide.Events.DTEEvents;
+                _ideEvents.OnBeginShutdown += InitializeVisualStudio;
+            });
         }
         void CloseVisualStudio()
         {
-            _ide.Quit();
+            TryExecute(_ide.Quit);
+        }
+
+        private static void TryExecute(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
     }
 }
---
+++ VsInterop/VsInterop/SolutionHelper.cs
@@ -0,0 +1,125 @@
+﻿using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VsInterop
+{
+    // https://github.com/codecadwallader/codemaid/blob/bd8b322ac58a4a114cdf3a8d646cee7336365c98/CodeMaid/Helpers/SolutionHelper.cs
+
+    internal static class SolutionHelper
+    {
+        #region Internal Methods
+
+        /// <summary>
+        /// Gets an enumerable set of all items of the specified type within the solution.
+        /// </summary>
+        /// <typeparam name="T">The type of item to retrieve.</typeparam>
+        /// <param name="solution">The solution.</param>
+        /// <returns>The enumerable set of all items.</returns>
+        internal static IEnumerable<T> GetAllItemsInSolution<T>(Solution solution)
+            where T : class
+        {
+            var allProjects = new List<T>();
+
+            if (solution != null)
+            {
+                allProjects.AddRange(GetItemsRecursively<T>(solution));
+            }
+
+            return allProjects;
+        }
+
+        /// <summary>
+        /// Gets items of the specified type recursively from the specified parent item. Includes
+        /// the parent item if it matches the specified type as well.
+        /// </summary>
+        /// <typeparam name="T">The type of item to retrieve.</typeparam>
+        /// <param name="parentItem">The parent item.</param>
+        /// <returns>The enumerable set of items within the parent item, may be empty.</returns>
+        internal static IEnumerable<T> GetItemsRecursively<T>(object parentItem)
+            where T : class
+        {
+            if (parentItem == null)
+            {
+                throw new ArgumentNullException("parentItem");
+            }
+
+            // Create a collection.
+            var projectItems = new List<T>();
+
+            // Include the parent item if it is of the desired type.
+            var desiredType = parentItem as T;
+            if (desiredType != null)
+            {
+                projectItems.Add(desiredType);
+            }
+
+            // Get all children based on the type of parent item.
+            var children = GetChildren(parentItem);
+
+            // Then recurse through all children.
+            foreach (var childItem in children)
+            {
+                projectItems.AddRange(GetItemsRecursively<T>(childItem));
+            }
+
+            return projectItems;
+        }
+        
+        internal static IEnumerable<ProjectItem> GetSimilarProjectItems(Solution solution, ProjectItem projectItem)
+        {
+            var allItems = GetAllItemsInSolution<ProjectItem>(solution);
+
+            return allItems.Where(x => x.Name == projectItem.Name && x.Kind == projectItem.Kind && x.Document.FullName == projectItem.Document.FullName);
+        }
+
+        #endregion Internal Methods
+
+        #region Private Methods
+
+        /// <summary>
+        /// Gets the children of the specified parent item if applicable.
+        /// </summary>
+        /// <param name="parentItem">The parent item.</param>
+        /// <returns>An enumerable set of children, may be empty.</returns>
+        private static IEnumerable<object> GetChildren(object parentItem)
+        {
+            // First check if the item is a solution.
+            var solution = parentItem as Solution;
+            if (solution != null && solution.Projects != null)
+            {
+                return solution.Projects.Cast<Project>().Cast<object>().ToList();
+            }
+
+            // Next check if the item is a project.
+            var project = parentItem as Project;
+            if (project != null && project.ProjectItems != null)
+            {
+                return project.ProjectItems.Cast<ProjectItem>().Cast<object>().ToList();
+            }
+
+            // Next check if the item is a project item.
+            var projectItem = parentItem as ProjectItem;
+            if (projectItem != null)
+            {
+                // Standard projects.
+                if (projectItem.ProjectItems != null)
+                {
+                    return projectItem.ProjectItems.Cast<ProjectItem>().Cast<object>().ToList();
+                }
+
+                // Projects within a solution folder.
+                if (projectItem.SubProject != null)
+                {
+                    return new[] { projectItem.SubProject };
+                }
+            }
+
+            // Otherwise return an empty array.
+            return new object[0];
+        }
+
+        #endregion Private Methods
+    }
+}
